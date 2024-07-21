75a95c6f1c5d3b67beb266ce5274cbba03b6ee61
Edwart Llanos <edwartlc@gmail.com>
2024-07-21 08:16:48 -0500

    primer commit

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+const clavesEncriptacion = {'e': 'enter', 'i': 'imes', 'a': 'ai', 'o': 'ober', 'u': 'ufat'};
+const caracteresValidos = ' abcdefghijklmnopqrstuvwxyz';
+
+function validarTexto(texto) {
+    let esValido = [];
+    for (let caracter of texto) {
+        for (let letra of caracteresValidos) {
+            if (caracter == letra) {
+                esValido.push(1);
+            }    
+        }
+    }
+    if (esValido.length == texto.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function convertirTexto(tipoConversion) {
+    let entrada = String(document.getElementById('textoUsuario').value.replaceAll('  ', ' '));
+    if (validarTexto(entrada) == false) {
+        alert('Debe ingresar solo letras minúsculas sin acentos y sin caracteres especiales.');
+    } else {
+        for (let [clave, valor] of Object.entries(clavesEncriptacion)) {
+            if (tipoConversion === 'encriptar') {
+                entrada = entrada.replaceAll(clave, valor);
+            } else {
+                entrada = entrada.replaceAll(valor, clave);
+            }
+        }
+        document.getElementById('salida').value = entrada;
+    }
+}
+
+function copiarTexto() {
+    let texto = document.getElementById('salida');
+    navigator.clipboard.writeText(texto.value);
+}
